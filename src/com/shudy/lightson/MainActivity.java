8beaf56e2bce388549898c01476cb0d8289f6c5f
Elías Ortiz <eliasortiz.pfc@gmail.com>
2013-07-15 12:39:52 +0200

    .

--- src/com/shudy/lightson/MainActivity.java
+++ src/com/shudy/lightson/MainActivity.java
@@ -6,15 +6,12 @@ import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.ads.AdRequest;
@@ -23,85 +20,71 @@ import com.google.ads.AdView;
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class MainActivity extends Activity {
-	
+
 	private boolean lighton;
 	private boolean lightsos;
-	
+
 	private ImageButton button;
 	private Button but_sos;
 	private Camera camera;
 	private Parameters params;
 	private TextView textView;
-	private Runnable runable_on;
-	private Runnable runable_off;
-	private Context context;
-
-	
-	//admob
+	// admob
 	private AdView ad;
 	private AdRequest adRequest;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
-		context = this;
-		
-		
-		ad = new AdView(this,AdSize.BANNER, "a151cac0b8cb6c2");
-		LinearLayout layout_publi = (LinearLayout)findViewById(R.id.LayoutPubli);
+
+		ad = new AdView(this, AdSize.BANNER, "a151cac0b8cb6c2");
+		LinearLayout layout_publi = (LinearLayout) findViewById(R.id.LayoutPubli);
 		layout_publi.addView(ad);
-		
-		//ADMOB
-		adRequest = new AdRequest();	
-		
+
+		// ADMOB
+		adRequest = new AdRequest();
+
 		setTestMode(true);
 		ad.loadAd(adRequest);
-		
-		
-		
-		button = (ImageButton)findViewById(R.id.Button1);
-		but_sos = (Button)findViewById(R.id.toggleSos);
-		textView = (TextView)findViewById(R.id.textview1);
+
+		button = (ImageButton) findViewById(R.id.Button1);
+		but_sos = (Button) findViewById(R.id.toggleSos);
+		textView = (TextView) findViewById(R.id.textview1);
 		textView.setVisibility(View.INVISIBLE);
 		lighton = false;
 		lightsos = false;
-		
+
 		try {
 			camera = Camera.open();
 			params = camera.getParameters();
-		}catch(Exception e) {
+		} catch (Exception e) {
 		}
-		
-		runable_on = new Runnable() {			
-			@Override
-			public void run() {				
-				params.setFlashMode(Parameters.FLASH_MODE_TORCH);
-				camera.setParameters(params);
-				camera.startPreview();
-			}
-		};
-		
-		runable_off = new Runnable() {			
-			@Override
-			public void run() {
-				params.setFlashMode(Parameters.FLASH_MODE_OFF);
-				camera.setParameters(params);
-				camera.startPreview();
-			}
-		};
-		
+
+		/*
+		 * runable_on = new Runnable() {
+		 * 
+		 * @Override public void run() {
+		 * params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		 * camera.setParameters(params); camera.startPreview(); } };
+		 * 
+		 * runable_off = new Runnable() {
+		 * 
+		 * @Override public void run() {
+		 * params.setFlashMode(Parameters.FLASH_MODE_OFF);
+		 * camera.setParameters(params); camera.startPreview(); } };
+		 */
+
 		button.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				if(!lighton) {
+				if (!lighton) {
 					params.setFlashMode(Parameters.FLASH_MODE_TORCH);
 					textView.setVisibility(View.VISIBLE);
-					
-				}
-				else
-				{
+
+				} else {
 					params.setFlashMode(Parameters.FLASH_MODE_OFF);
 					textView.setVisibility(View.INVISIBLE);
 				}
@@ -111,15 +94,14 @@ public class MainActivity extends Activity {
 			}
 		});
 		but_sos.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				SosAsyntask sos = new SosAsyntask();
 				sos.execute();
-				if(!lightsos) {
-				}
-				else {
-					
+				if (!lightsos) {
+				} else {
+
 				}
 				lightsos = !lightsos;
 			}
@@ -127,14 +109,14 @@ public class MainActivity extends Activity {
 		EasyTracker.getInstance().activityStart(this);
 	}
 
-private void setTestMode(boolean test) {
-	
-	if(test) {
-		adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
-		adRequest.addTestDevice("6D7C0138CC30E340358299979DDD11B9"); //GALAXY S3 --Elías
-	}
-	
-		
+	private void setTestMode(boolean test) {
+
+		if (test) {
+			adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
+			// GALAXY S3 --Elías
+			adRequest.addTestDevice("6D7C0138CC30E340358299979DDD11B9");
+		}
+
 	}
 
 	@Override
@@ -145,14 +127,13 @@ private void setTestMode(boolean test) {
 
 	@Override
 	protected void onStop() {
-		if(camera != null) {
+		if (camera != null) {
 			camera.release();
 		}
 		super.onStop();
-		EasyTracker.getInstance().activityStop	(this);
+		EasyTracker.getInstance().activityStop(this);
 	}
-	
-	
+
 	@Override
 	protected void onDestroy() {
 		ad.destroy();
@@ -164,75 +145,84 @@ private void setTestMode(boolean test) {
 		camera.setParameters(params);
 		camera.startPreview();
 	}
-	
+
 	public void ligthOff() {
 		params.setFlashMode(Parameters.FLASH_MODE_OFF);
 		camera.setParameters(params);
 		camera.startPreview();
 	}
-	
+
+	public void dash() {
+		try {
+			ligthOn();
+			Thread.sleep(750);
+			ligthOff();
+			Thread.sleep(250);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void dot() {
+		try {
+			ligthOn();
+			Thread.sleep(250);
+			ligthOff();
+			Thread.sleep(250);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private class SosAsyntask extends AsyncTask<Void, Void, Boolean> {
-		
+
 		@Override
 		public Boolean doInBackground(Void... params) {
-			
-			try{
-				
-				
+			try {
+				dot();
+				dot();
+				dot();
 				Thread.sleep(250);
-				
+				dash();
+				dash();
+				dash();
 				Thread.sleep(250);
-				//S
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-				//O
-				ligthOn();
-				Thread.sleep(1000);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(1000);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(1000);
-				ligthOff();
-				Thread.sleep(500);
-				//S
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-				ligthOn();
-				Thread.sleep(250);
-				ligthOff();
-				Thread.sleep(500);
-				
-			}catch(Exception e){
-			 return false;		
+				dot();
+				dot();
+				dot();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
 			}
-			
+			/*
+			 * try{
+			 * 
+			 * 
+			 * Thread.sleep(250);
+			 * 
+			 * Thread.sleep(250); //S ligthOn(); Thread.sleep(250); ligthOff();
+			 * Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(250); ligthOff(); Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(250); ligthOff(); Thread.sleep(500);
+			 * 
+			 * //O ligthOn(); Thread.sleep(1000); ligthOff(); Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(1000); ligthOff(); Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(1000); ligthOff(); Thread.sleep(500); //S
+			 * ligthOn(); Thread.sleep(250); ligthOff(); Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(250); ligthOff(); Thread.sleep(500);
+			 * 
+			 * ligthOn(); Thread.sleep(250); ligthOff(); Thread.sleep(500);
+			 * 
+			 * }catch(Exception e){ return false; }
+			 */
+
 			return true;
 		}
 
@@ -243,5 +233,5 @@ private void setTestMode(boolean test) {
 			}
 		}
 	}
-	
+
 }
